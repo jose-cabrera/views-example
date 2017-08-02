20d0ce301067919843995651e9b4bf52c19a072b
Jose Cabrera <deltadax18@gmail.com>
2017-08-01 20:21:17 -0600

    feat: LinearLayout Activity

---
+++ app/src/main/java/com/deltadax/views_example/MainActivity.java
@@ -0,0 +1,16 @@
+package com.deltadax.views_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Activity donde implementaremos Linear layout para crear un formulario sencillo
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
