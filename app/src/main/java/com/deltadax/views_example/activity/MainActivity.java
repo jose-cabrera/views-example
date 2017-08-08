aa0b267037ea9525327204ba7e23e7e06c122e46
Jose Cabrera <deltadax18@gmail.com>
2017-08-07 19:10:20 -0600

    Poner a Nancy al dia

--- app/src/main/java/com/deltadax/views_example/activity/MainActivity.java
+++ app/src/main/java/com/deltadax/views_example/activity/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.AppCompatButton;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
--- app/src/main/java/com/deltadax/views_example/activity/RecycleListActivity.java
+++ app/src/main/java/com/deltadax/views_example/activity/RecycleListActivity.java
@@ -1,19 +1,24 @@
 package com.deltadax.views_example.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.deltadax.views_example.R;
-import com.deltadax.views_example.entity.ListItem;
 
-import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecycleListActivity extends AppCompatActivity {
+
+    @BindView(R.id.recylcer)
+    RecyclerView recylcer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_list);
+        ButterKnife.bind(this);
     }
 
 
